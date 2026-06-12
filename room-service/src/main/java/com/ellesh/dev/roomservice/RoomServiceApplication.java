b8bc85c067cf1634a540eb1ac4186353e06e9f90
kelothellesh@gmail.com <kelothellesh@gmail.com>
2026-06-12 17:16:53 +0530

    client-discovery

--- room-service/src/main/java/com/ellesh/dev/roomservice/RoomServiceApplication.java
+++ room-service/src/main/java/com/ellesh/dev/roomservice/RoomServiceApplication.java
@@ -2,7 +2,9 @@ package com.ellesh.dev.roomservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RoomServiceApplication {
 
