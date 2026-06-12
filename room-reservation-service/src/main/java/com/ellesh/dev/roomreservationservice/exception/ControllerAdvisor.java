8ef450c16a49e945dda253b54843f72bb29ef27d
kelothellesh@gmail.com <kelothellesh@gmail.com>
2026-06-12 18:29:54 +0530

    version update

---
+++ room-reservation-service/src/main/java/com/ellesh/dev/roomreservationservice/exception/ControllerAdvisor.java
@@ -0,0 +1,27 @@
+package com.ellesh.dev.roomreservationservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RoomNotFoundException.class)
+    public ResponseEntity<Object> handleRoomNotFoundException(
+            RoomNotFoundException ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Room not found");
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
