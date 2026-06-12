f9bacaecff0a50a63288eaf624e163ca84a4a9b4
kelothellesh@gmail.com <kelothellesh@gmail.com>
2026-06-12 18:04:18 +0530

    discover room-reservation service

--- room-reservation-service/src/main/java/com/ellesh/dev/roomreservationservice/RoomReservationServiceApplication.java
+++ room-reservation-service/src/main/java/com/ellesh/dev/roomreservationservice/RoomReservationServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RoomReservationServiceApplication {
 
